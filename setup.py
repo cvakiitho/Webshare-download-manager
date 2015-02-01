addda5d7615b4572c42a01099c1f1c73c192c6d7
cvakiitho <cvakiitho@gmail.com>
2015-02-01 13:32:01 +0100

    License + pipy related files + relocating tests

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 # -*- coding: UTF=8 -*-
 __author__ = 'Tomas Hartmann'
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='webshare-download-manager',
-      version='0.1',
+      version='0.1.3',
       description='Download manager for webshare.cz site. Flask + requests.',
       url='https://github.com/cvakiitho/Webshare-download-manager',
       author='Tomas Hartmann',
       author_email='cvakiitho@gmail.com',
       license='MIT',
-      packages=['webshare-download-manager'],
-      zip_safe=False)
+      packages=find_packages(),
+      zip_safe=False,
+      install_requires=[
+          'Flask>=0.10.1',
+          'Jinja2>=2.7.3',
+          'requests>=2.5.1'
+      ]
+)
